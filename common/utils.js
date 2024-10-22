b901a5af7ced156583ffafb8e6f8d11cad56f60f
Benjamin Callaway <benjaminc247@outlook.com>
2024-10-21 20:14:03 -0700

    throw error instead of string add abort signal to fetch text

--- common/utils.js
+++ common/utils.js
@@ -1,25 +1,34 @@
 /**
  * Async fetch text data from file, throw error if response is not ok.
  * @param {string} fileName - name of data file to fetch
+ * @param {Object} [opts] - destructured options param
+ * @param {AbortSignal} [opts.signal] - abort signal object
+ * @param {number} [opts.timeout] - fetch timeout in ms
+ * @returns {Promise<string>} - promise to fetch text from file
  */
-export async function fetchText(fileName, { timeout = 30000 } = {}) {
-  const response = await fetch(fileName, { signal: AbortSignal.timeout(timeout) });
+export async function fetchText(fileName, { signal = undefined, timeout = 30000 } = {}) {
+  const timeoutSignal = AbortSignal.timeout(timeout);
+  const fetchSignal = signal ? AbortSignal.any([timeoutSignal, signal]) : timeoutSignal;
+  const response = await fetch(fileName, { signal: fetchSignal });
   if (!response.ok)
-    throw `${response.status} ${response.statusText}`;
+    throw new Error(`${response.status} ${response.statusText}`);
   return response.text();
 }
 
 /**
  * Async fetch json data from file, throw error if response is not ok.
  * @param {string} fileName - name of data file to fetch
- * @returns {Promise<object>}
+ * @param {Object} [opts] - destructured options param
+ * @param {AbortSignal} [opts.signal] - abort signal object
+ * @param {number} [opts.timeout] - fetch timeout in ms
+ * @returns {Promise<Object>} - promise to fetch json object from file
  */
 export async function fetchJson(fileName, { signal = undefined, timeout = 30000 } = {}) {
   const timeoutSignal = AbortSignal.timeout(timeout);
-  signal = signal ? AbortSignal.any([timeoutSignal, signal]) : timeoutSignal;
-  const response = await fetch(fileName, { signal: signal });
+  const fetchSignal = signal ? AbortSignal.any([timeoutSignal, signal]) : timeoutSignal;
+  const response = await fetch(fileName, { signal: fetchSignal });
   if (!response.ok)
-    throw `${response.status} ${response.statusText}`;
+    throw new Error(`${response.status} ${response.statusText}`);
   return response.json();
 }
 
